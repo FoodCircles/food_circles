b60753024f69436b024d9eeb00cfa95bf227521a
Haseeb Khan <haseeb@tkxel.com>
2013-01-29 12:29:13 -0500

    Monthly invoice custom fields modification completed.

--- public/javascripts/change_view.js
+++ public/javascripts/change_view.js
@@ -1,5 +1,7 @@
 function change_view(vid,months_before){
 
+    $("#loading").show();
+
     $.ajax({
         type: 'GET',
         url: '/monthly_invoice/new_layout?vid='+vid+'&months_before='+months_before+'&input_note='+input_note,
@@ -16,10 +18,12 @@ function change_view(vid,months_before){
             }
         }
     });
+    $("#loading").hide();
 
 }
 function change_request(vid,months_before,fee_message,fee_cost,input_note){
 
+    $("#loading").show();
     $.ajax({
         type: 'GET',
         url: '/monthly_invoice/custom_invoice?vid='+vid+'&months_before='+months_before+'&fee_message='+fee_message+'&fee_cost='+fee_cost+'&input_note='+input_note,
@@ -36,5 +40,6 @@ function change_request(vid,months_before,fee_message,fee_cost,input_note){
             }
         }
     });
+    $("#loading").hide();
 
 }
