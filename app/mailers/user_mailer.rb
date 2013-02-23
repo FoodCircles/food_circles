38108945dcc2dacb1e645102374066cccf592a58
Haseeb Khan <haseeb@tkxel.com>
2013-02-23 15:23:37 -0500

    Email font

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -26,23 +26,25 @@ class UserMailer < ActionMailer::Base
       reply_to 'support@foodcircles.net'
       html_part do
         content_type 'text/html; charset=UTF-8'
-        body "<table width = '550px'><tr><td><p style= text-align: justify>Print this email or just show it off on a fancy electronic device.</p>
+        body "<table width = '550px'><tr><td><p style= text-align: justify; font-size:12pt; font-family:Arial>Print this email or just show it off on a fancy electronic device.</p>
               <p style= text-align: justify>
-              confirmation code: #{r.coupon}<br>
-              good for: #{deal.name}<br>
-              only at: #{r.venue.name.capitalize.gsub(/\'/,"\\\'") }<br>
-              with a minimum of: #{user_reservation.num_diners} diners<br>
-              expiring: #{7.days.from_now.to_date}</p><br>
-              <b>3 steps to redeem:</b><p><br>
+              <b>confirmation code:</b> #{r.coupon}<br>
+              <b>good for:</b> #{deal.name}<br>
+              <b>only at:</b> #{r.venue.name.capitalize.gsub(/\'/,"\\\'") }<br>
+              <b>with a minimum of:</b> #{user_reservation.num_diners} diners<br>
+              <b>expiring:</b> #{7.days.from_now.to_date}</p><br>
+              <b>3 steps to redeem:</b>
+              <p>
               <b>1)</b> Show server this message before you order.  They should jot your code down and confirm.<br>
               <b>2)</b> Order regular food or drink for each person in party.<br>
-              <b>3)</b> Your \"Buy One, Feed One\"  item(s) will be taken off your final receipt.</p>
+              <b>3)</b> Your \"Buy One, Feed One\"  item(s) will be taken off your final receipt.
+              </p><br>
               Enjoy!
               <br>
               <br>
-              Contact support at <b>support@foodcircles.net</b> if you have any concerns or questions whatsoever.<br>
+              Contact support at <b>support@foodcircles.net</b> if you have any concerns or questions whatsoever.<br><br><br>
               <h3><u>FOR SERVERS:</u></h3>
-              <p style= text-align: justify ><b>Write down the confirmation code on the table's receipt or your POS system</b>.  Place a  \"Buy One, Feed One\" emblem on the guest's table, and mark a tally on your chalkboard (if available).  Call us at 312 945 8627 with any questions!</p></td></tr></table>"
+              <p style= text-align: justify;><b>Write down the confirmation code on the table's receipt or your POS system</b>.  Place a  \"Buy One, Feed One\" emblem on the guest's table, and mark a tally on your chalkboard (if available).  Call us at 312 945 8627 with any questions!</p></td></tr></table>"
         end
     end
   end
