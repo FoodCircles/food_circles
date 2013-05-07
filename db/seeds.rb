009c6184197c16af1a9cd1586269aabe0d825481
Jaime Blalock <zjeb40@goldmail.etsu.edu>
2013-05-07 14:33:26 -0400

    seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+venue = Venue.create(:name => "Georgio's pizza")
+Offer.create(:name => "Free Desserts", :venue => venue)
+venue = Venue.create(:name => "Hopcat")
+Offer.create(:name => "Free Appetizers", :venue => venue)
+venue = Venue.create(:name => "Sanchez Bistro")
+Offer.create(:name => "Free Loaded Crack Fries", :venue => venue)
+
