dbfe6794208a8795f01ba0efd3e8b4de092b4b3b
Jaime Blalock <zjeb40@goldmail.etsu.edu>
2013-05-10 16:27:14 -0400

    add social butterfly email functionality

--- app/controllers/socialbutterflies_controller.rb
+++ app/controllers/socialbutterflies_controller.rb
@@ -1,11 +1,6 @@
 class SocialbutterfliesController < ApplicationController
   def index
-  	
+  	UserMailer.social_butterfly(params[:facebook]).deliver    	
   end 
-
-  def submit
-  	#@socialbutterflies = Socialbutterflies.find(params[:facebook])
-    UserMailer.social_butterfly("http://www.facebook.com/Siesna").deliver    	
-  end
 
 end
