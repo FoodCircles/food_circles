879d4773d065cf14d2a473f4bde100cf720c3cdf
Jaime Blalock <zjeb40@goldmail.etsu.edu>
2013-05-10 16:46:10 -0400

    add database functionality to For Restaurants

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -6,8 +6,15 @@ class RestaurantsController < ApplicationController
   end
 
   def signup
-  	UserMailer.restaurant_notify(params[:email], params[:name]).deliver
-  	UserMailer.restaurant_signup(params[:email], params[:name]).deliver
+  	email = params[:email]
+  	name = params[:name]
+  	UserMailer.restaurant_notify(email, name).deliver
+  	UserMailer.restaurant_signup(email, name).deliver
+
+  	@n = Notification.create
+	@n.content = "Name: #{name}, Email: #{email}"
+	@n.ticker = "A restaurant signup"
+	@n.save
   end
 
 end
