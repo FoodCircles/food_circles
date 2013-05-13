92c03776400405a7ee644167f572cac6c05054e6
Will Holcomb <wholcomb@synaptian.com>
2013-05-13 12:17:22 -0400

    merging changes from development

--- db/schema.rb
+++ db/schema.rb
@@ -78,12 +78,8 @@ ActiveRecord::Schema.define(:version => 20130508201332) do
     t.string   "name"
     t.text     "details"
     t.integer  "min_diners"
-    t.datetime "created_at",         :null => false
-    t.datetime "updated_at",         :null => false
-    t.string   "image_file_name"
-    t.string   "image_content_type"
-    t.integer  "image_file_size"
-    t.datetime "image_updated_at"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   create_table "open_times", :force => true do |t|
