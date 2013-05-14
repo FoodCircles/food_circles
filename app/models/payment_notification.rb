af83dd5c8bf2f2f943eae7c10ae19871743f38f2
Will Holcomb <wholcomb@synaptian.com>
2013-05-14 15:11:41 -0400

    added email validations to payment_notification model

--- app/models/payment_notification.rb
+++ app/models/payment_notification.rb
@@ -1,3 +1,7 @@
 class PaymentNotification < ActiveRecord::Base
+  includes Validators
+
   attr_accessible :parameters, :paypal_id, :status, :transaction_id, :address_city, :address_country, :address_name, :address_state, :address_street, :address_zip, :first_name, :invoice, :last_name, :mc_currency, :mc_gross, :payer_email, :payer_status, :payment_date, :payment_type, :txn_id, :verify_sign, :user_id
+  validates :payer_email, :on => :update, :'validators/email' => true
+  validates :payer_email, :on => :create, :allow_nil => true, :'validators/email' => true
 end
