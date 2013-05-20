9076ff2f38b56db68d3101ac3eb837b77a428f4b
Will Holcomb <will@dhappy.org>
2013-05-20 15:25:36 -0400

    adding s3 authentication info

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -77,4 +77,13 @@ Foodcircles::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+  config.paperclip_defaults = {
+  :storage => :s3,
+    :s3_credentials => {
+    :bucket => 'foodcircles',
+    :access_key_id => '0BRVRK6FWACPYQ6BCSR2',
+    :secret_access_key => 'DXbUDzUdDIvyA9x3jajgDoM4/szI783MIujFp39E'
+    }
+  }
 end
