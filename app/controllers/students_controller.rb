3ed51a50a2799e8f92fe001a1007e8862e03342a
Jaime Blalock <zjeb40@goldmail.etsu.edu>
2013-05-21 10:50:45 -0400

    fix merge conflict

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,6 +1,5 @@
 class StudentsController < ApplicationController
-<<<<<<< HEAD
-=======
+
   def index
     if params[:students_email]
       signup
@@ -11,5 +10,4 @@ class StudentsController < ApplicationController
     UserMailer.students_notify(params[:students_email]).deliver
     UserMailer.students_signup(params[:students_email]).deliver
   end
->>>>>>> 5d69c31e3bbab8b34e3c25f00cfb83a158f4508e
 end
