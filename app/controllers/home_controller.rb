5a4a4c40e13259625004e51661393b2b83c821b9
Will Holcomb <will@dhappy.org>
2013-05-21 13:42:28 -0400

    adding weekly meal goals

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,23 @@
 class HomeController < ApplicationController
   def index
-  	#UserMailer.signupsuccess
-  	#render :text=>'here2' and return
-  	@offers = Offer.limit(9)
+    #UserMailer.signupsuccess
+    #render :text=>'here2' and return
+    @offers = Offer.limit(9)
+    
+    @progress = 0
+    Stripe::Charge.all.each do |charge|
+      if Time.at(charge.created) > Time.now - 1.week
+        @progress += charge.amount
+      end
+    end
+    @progress /= 100
+
+    @total_vouchers = 0
+    Offer.all.each do |offer|
+      @total_vouchers += offer.price * offer.total
+    end
+    @total_vouchers = @total_vouchers.round
+    @adjusted_total = 3 * @total_vouchers / 4
   end
 
   def cater
