db191d080907a9c33304c079a8b2be9088f42af5
Will Holcomb <will@dhappy.org>
2013-05-22 15:32:38 -0400

    importing colorbox into homepage javascript

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,6 +1,7 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 //= require jquery.carouFredSel-6.2.1-packed
+//= require jquery.colorbox
 
 (function($){
 
