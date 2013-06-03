2cfc746f216887f2662f5f9cf0bbb222e52023ce
Will Holcomb <will@dhappy.org>
2013-06-03 14:08:26 -0400

    generating vouchers

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,16 +9,13 @@ user = User.find_or_create_by_email("wholcomb@synaptian.com")
 
 venue = Venue.create(:name => "Georgio's pizza")
 offer = Offer.create(:name => "Free Desserts", :venue => venue)
-Voucher.create(offer: offer, start: Time.now, :end => Time.now + 2.years, total: 10, available: rand(11))
 Payment.create(user: user, offer: offer, amount: 10)
 
 venue = Venue.create(:name => "Hopcat")
 offer = Offer.create(:name => "Free Appetizers", :venue => venue)
-Voucher.create(offer: offer, start: Time.now, :end => Time.now + 2.years, total: 10, available: rand(11))
 Payment.create(user: user, offer: offer, amount: 10)
 
 venue = Venue.create(:name => "Sanchez Bistro")
 offer = Offer.create(:name => "Free Loaded Crack Fries", :venue => venue)
-Voucher.create(offer: offer, start: Time.now, :end => Time.now + 2.years, total: 10, available: rand(11))
 Payment.create(user: user, offer: offer, amount: 10)
 
