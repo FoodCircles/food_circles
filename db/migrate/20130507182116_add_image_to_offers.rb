682aa2154423a1817ae480f3a39470c37193d518
Will Holcomb <will@dhappy.org>
2013-06-03 14:09:09 -0400

    manually adding image fields

--- db/migrate/20130507182116_add_image_to_offers.rb
+++ db/migrate/20130507182116_add_image_to_offers.rb
@@ -3,7 +3,11 @@ class AddImageToOffers < ActiveRecord::Migration
   end
 
   def self.up
-    add_attachment :offers, :image
+    # add_attachment :offers, :image
+    add_column :offers, :image_file_name, :string
+    add_column :offers, :image_content_type, :string
+    add_column :offers, :image_file_size, :integer
+    add_column :offers, :image_updated_at, :datetime 
   end
 
   def self.down
