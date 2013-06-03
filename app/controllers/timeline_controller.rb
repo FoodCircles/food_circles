b551685e6f7342ee6269a8d62ce37c0a376550b5
Will Holcomb <will@dhappy.org>
2013-06-03 14:04:50 -0400

    totaling vouchers

--- app/controllers/timeline_controller.rb
+++ app/controllers/timeline_controller.rb
@@ -1,6 +1,9 @@
 class TimelineController < ApplicationController
   def index
-    @weekly_total = Reservation.where("created_at > ?", Time.now - 1.week)
+    @weekly_total = Payment.where("created_at > ?", Time.now - 1.week).collect{ |p| p.amount }.sum
+    current_vouchers = Voucher.where("start_date <= ? and end_date >= ?", Time.now, Time.now)
+    @total_vouchers = current_vouchers.collect{ |v| v.total }.sum
+    @available_vouchers = current_vouchers.collect{ |v| v.available }.sum
     @payments = Payment.order(:created_at).limit(3)
   end
 end
