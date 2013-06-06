687e248f9dc293decd0b00ad86ea3315495ddf57
Jaime Blalock <zjeb40@goldmail.etsu.edu>
2013-06-06 16:07:43 -0400

    employing texting users the app

--- app/controllers/popups_controller.rb
+++ app/controllers/popups_controller.rb
@@ -4,4 +4,14 @@ class PopupsController < ApplicationController
   def deal_popup_not_logged
     @offer = Offer.find(params[:id])
   end
+
+  def app_popup()
+  	if params[:phone]
+      if type == 'iphone'
+        Home.sendText(phone, "Download the FoodCircles app! https://itunes.apple.com/us/app/foodcircles/id526107767?mt=8")
+      elsif type == 'android'
+        Home.sendText(phone, "Download the FoodCircles app! https://play.google.com/store/apps/details?id=co.foodcircles")
+      end
+    end
+  end
 end
