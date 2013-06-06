828de1d162aaad890578e81411a5e6681ff0b626
Will Holcomb <will@dhappy.org>
2013-06-06 17:41:30 -0400

    adding coupon code

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130529211752) do
+ActiveRecord::Schema.define(:version => 20130606200744) do
 
   create_table "categories", :force => true do |t|
     t.string   "name"
@@ -133,6 +133,7 @@ ActiveRecord::Schema.define(:version => 20130529211752) do
     t.datetime "created_at",          :null => false
     t.datetime "updated_at",          :null => false
     t.integer  "offer_id"
+    t.string   "code"
   end
 
   create_table "rails_admin_histories", :force => true do |t|
