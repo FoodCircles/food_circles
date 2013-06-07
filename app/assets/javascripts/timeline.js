7075ed3aa4df9342af6ebb797a6860bad7ae54d8
Will Holcomb <will@dhappy.org>
2013-06-07 13:10:39 -0400

    using popup code from homepage

--- app/assets/javascripts/timeline.js
+++ app/assets/javascripts/timeline.js
@@ -1,17 +1,4 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 //= require jquery.colorbox
-
-$( function() {
-    $(document).on( 'click', '.recieptlink', function() {
-	event.preventDefault()
-	$.colorbox( {
-	    href: $(this).attr('href'),
-            onComplete: function() {
-                jQuery.colorbox.resize()
-            },
-            height: 600,
-            inline: true,
-	} )
-    } )
-} )
+//= require home
--- app/controllers/popups_controller.rb
+++ app/controllers/popups_controller.rb
@@ -5,18 +5,17 @@ class PopupsController < ApplicationController
     @offer = Offer.find(params[:id])
   end
 
-<<<<<<< HEAD
   def reciept
     @payment = Payment.find(params[:id])
-=======
+  end
+
   def app_popup()
-  	if params[:phone]
+    if params[:phone]
       if type == 'iphone'
         Home.sendText(phone, "Download the FoodCircles app! https://itunes.apple.com/us/app/foodcircles/id526107767?mt=8")
       elsif type == 'android'
         Home.sendText(phone, "Download the FoodCircles app! https://play.google.com/store/apps/details?id=co.foodcircles")
       end
     end
->>>>>>> ce87afdb801cdf3d8a673d4c8d584a4e4dc31b1e
   end
 end
