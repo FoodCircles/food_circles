78d2854ded1ae74ca5940d59a0c5130f1a8543fa
Pablo Merino <pablo.perso1995@gmail.com>
2013-07-07 18:30:39 -0500

    Fixing some javascript for "Pay what you want" issue

--- app/assets/javascripts/stripe_payment.js
+++ app/assets/javascripts/stripe_payment.js
@@ -1,28 +1,29 @@
-$( function() {
-    $(document).on('click', '#dealbuy', function(event){
-	event.preventDefault();
-
-        var card = {
-            number:   $("#card-number").val(),
-            expMonth: $("#exp-month").val(),
-            expYear:  $("#exp-year").val(),
-            cvc:      $("#cvc").val()
-        }
-        Stripe.createToken(card, function(status, response) {
-          $("a#dealbuy").text("Loading...");
-          if(response.error) {
-            console.log(status + " - " + response.error.message);
-          }
-            
-          if (status === 200) {
-            $("[name='stripe_token']").val(response.id)
-            $("#dealform").submit()
-          } else {
-            $("a#dealbuy").text("Error!");
-            $("#stripe-error-message").text(response.error.message)
-            $("#credit-card-errors").show()
-            $("#user_submit").attr("disabled", false)
-          }
-        });
-    })
-} )
+$(function() {
+  $(document).on('click', '#dealbuy', function(event){
+  	event.preventDefault();
+    $("a#dealbuy").text("Loading...");
+    var card = {
+      number:   $("#card-number").val(),
+      expMonth: $("#exp-month").val(),
+      expYear:  $("#exp-year").val(),
+      cvc:      $("#cvc").val()
+    }
+        
+    Stripe.createToken(card, function(status, response) {
+    
+      if(response.error) {
+        console.log(status + " - " + response.error.message);
+      }
+      
+      if (status === 200) {
+        $("[name='stripe_token']").val(response.id)
+        $("#dealform").submit()
+      } else {
+        $("a#dealbuy").text("Error!");
+        $("#stripe-error-message").text(response.error.message)
+        $("#credit-card-errors").show()
+        $("#user_submit").attr("disabled", false)
+      }
+    });
+  })
+})
