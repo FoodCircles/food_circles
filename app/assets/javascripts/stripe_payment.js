592618a2899f050ae9767862a8fb56d1a8228296
Pablo Merino <pablo.perso1995@gmail.com>
2013-07-07 15:58:03 -0500

    Fixed issue "Pay what you want" #3

--- app/assets/javascripts/stripe_payment.js
+++ app/assets/javascripts/stripe_payment.js
@@ -9,16 +9,20 @@ $( function() {
             cvc:      $("#cvc").val()
         }
         Stripe.createToken(card, function(status, response) {
-            $("a#dealbuy").text("Loading...");
-            if (status === 200) {
-                $("[name='stripe_token']").val(response.id)
-                $("#dealform").submit()
-            } else {
-                $("a#dealbuy").text("Error!");
-                $("#stripe-error-message").text(response.error.message)
-                $("#credit-card-errors").show()
-                $("#user_submit").attr("disabled", false)
-            }
+          $("a#dealbuy").text("Loading...");
+          if(response.error) {
+            console.log(status + " - " + response.error.message);
+          }
+            
+          if (status === 200) {
+            $("[name='stripe_token']").val(response.id)
+            $("#dealform").submit()
+          } else {
+            $("a#dealbuy").text("Error!");
+            $("#stripe-error-message").text(response.error.message)
+            $("#credit-card-errors").show()
+            $("#user_submit").attr("disabled", false)
+          }
         });
     })
 } )
