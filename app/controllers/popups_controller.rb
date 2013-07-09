67c25924f2cad07fba41cddcf10c4fcdc1065d63
Pablo Merino <pablo.perso1995@gmail.com>
2013-07-08 19:12:11 -0500

    Fixing issue "Share a purchase online"

--- app/controllers/popups_controller.rb
+++ app/controllers/popups_controller.rb
@@ -11,6 +11,17 @@ class PopupsController < ApplicationController
 
   def reciept
     @payment = Payment.find(params[:id])
+    @twitter_status = Rack::Utils.escape("Fed #{@payment.amount.to_i} #{@payment.amount.to_i > 1 ? "children" : "child"} in need simply by eating out. Picked out #{@payment.offer.venue.name} via @foodcircles. #bofo http://joinfoodcircles.org/#{@payment.offer.venue.slug}")
+    @facebook_status = {
+      :url => "http://joinfoodcircles.org/#{@payment.offer.venue.slug}",
+      :image => "http://www.foodcircles.net/assets/home/main_intro_high.png",
+      :title => Rack::Utils.escape("Bought one, fed #{@payment.amount.to_i}"),
+      :summary => Rack::Utils.escape("Fed #{@payment.amount.to_i} #{@payment.amount.to_i > 1 ? "children" : "child"} simply by eating out. Picked out #{@payment.offer.venue.name} via FoodCircles.")
+    }
+    @email = {
+      :subject => "Bought one, fed #{@payment.amount.to_i}",
+      :body => "Hey, just bought a voucher for #{@payment.offer.venue.name}. Proceeds from our meal are going to feed #{@payment.amount.to_i} #{@payment.amount.to_i > 1 ? "children" : "child"} in need. Where/when do you want to meet?"
+    }
   end
 
   def app_popup()
