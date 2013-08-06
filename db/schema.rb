19193c4c925fb381d8fd1837cdd78a9c42d9116b
Pablo Merino <pablo.perso1995@gmail.com>
2013-08-06 21:54:30 +0200

    Fixing the cookie overflow (WHEN DEPLOYING NEEDS A DATABASE MIGRATION)

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130728221106) do
+ActiveRecord::Schema.define(:version => 20130806195316) do
 
   create_table "categories", :force => true do |t|
     t.string   "name"
@@ -202,6 +202,16 @@ ActiveRecord::Schema.define(:version => 20130728221106) do
   end
 
   add_index "reviews", ["venue_id"], :name => "index_reviews_on_venue_id"
+
+  create_table "sessions", :force => true do |t|
+    t.string   "session_id", :null => false
+    t.text     "data"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  add_index "sessions", ["session_id"], :name => "index_sessions_on_session_id"
+  add_index "sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
 
   create_table "socialbutterflies", :force => true do |t|
     t.string   "facebook"
