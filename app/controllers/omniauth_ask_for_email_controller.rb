695b7bd244dcbf40a01bf84eac1c9e23ca6d7de1
Pablo Merino <pablo.perso1995@gmail.com>
2013-08-07 21:55:21 +0200

    Small fix

--- app/controllers/omniauth_ask_for_email_controller.rb
+++ app/controllers/omniauth_ask_for_email_controller.rb
@@ -1,5 +1,5 @@
 class OmniauthAskForEmailController < ApplicationController
-  before_filter :verify
+  before_filter :authenticate_user!
   def index
   end
 
@@ -9,14 +9,6 @@ class OmniauthAskForEmailController < ApplicationController
       redirect_to root_path
     else
       redirect_to :back, :alert => "There was an error (email #{current_user.errors.messages[:email].first})"
-    end
-  end
-  
-  def verify
-    if authenticate_user!
-      if !current_user.email.blank?
-        redirect_to root_path
-      end
     end
   end
 end
