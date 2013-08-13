5fa066427a6a30ec09eaff91733d5e56e46ec0ad
lacides <lcharris89@gmail.com>
2013-08-13 17:35:18 -0500

    configure mailchimp globally

--- app/controllers/api/general_controller.rb
+++ app/controllers/api/general_controller.rb
@@ -2,7 +2,7 @@ class Api::GeneralController < ApplicationController
   def get_mailchimp_users
     
     begin
-      gb = Gibbon::API.new("d915933790ef85a69849f5c12a5162d7-us4")
+      gb = Gibbon::API.new
       number_of_users = gb.lists.list(:filters => {:list_name => "FoodCircles Members"})["data"][0]["stats"]["member_count"]
       render :json => {:error => false, :content => number_of_users}
     rescue Timeout::Error => e
---
+++ config/initializers/gibbon_config.rb
@@ -0,0 +1,2 @@
+ Gibbon::API.api_key = "d915933790ef85a69849f5c12a5162d7-us4"
+ 
