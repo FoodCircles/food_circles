3fb49c629449092d505a0009d39d01ed1fad09f0
lacides <lcharris89@gmail.com>
2013-08-21 17:10:34 -0500

    venues api does a proximity search

--- app/controllers/api/venues_controller.rb
+++ app/controllers/api/venues_controller.rb
@@ -1,6 +1,9 @@
 class Api::VenuesController < ApplicationController
   def show
-    @venue = Venue.where(:latlon => Venue.rgeo_factory_for_column(:latlon).point(params[:lat], params[:lon]))
-    render :json => {:error => false, :content => @venue.as_json }
+    meters_on_a_mille = 1609.344
+    fifty_milles_as_meters = 50*meters_on_a_mille
+    point = Venue.rgeo_factory_for_column(:latlon).point(params[:lat], params[:lon])
+    @venues = Venue.where(["ST_DWithin(latlon, ?, #{fifty_milles_as_meters})", point])
+    render :json => {:error => false, :content => @venues.as_json }
   end
 end
