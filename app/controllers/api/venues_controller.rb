a463aaab96525c2f66e1f88aab1db2bde8cad951
lacides <lcharris89@gmail.com>
2013-08-28 11:16:19 -0500

    include distance on api/venues

--- app/controllers/api/venues_controller.rb
+++ app/controllers/api/venues_controller.rb
@@ -4,6 +4,6 @@ class Api::VenuesController < ApplicationController
     fifty_milles_as_meters = 50*meters_on_a_mille
     point = Venue.rgeo_factory_for_column(:latlon).point(params[:lat], params[:lon])
     @venues = Venue.where(["ST_DWithin(latlon, ?, #{fifty_milles_as_meters})", point])
-    render :json => {:error => false, :content => @venues.as_json }
+    render :json => {:error => false, :content => @venues.as_json(include_distance_to: point) }
   end
 end
