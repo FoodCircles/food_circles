075c308db84cdb879fbd565efaef95f2fb728e30
Joe Woodward <woodward.jp@gmail.com>
2013-09-07 16:44:53 +0100

    add debugging for production testing

--- app/controllers/newsletters_controller.rb
+++ app/controllers/newsletters_controller.rb
@@ -8,15 +8,17 @@ class NewslettersController < ApplicationController
         list_id = gb.lists.list(:filters => {:list_name => Rails.configuration.mailchimp_list_name})["data"][0]["id"]
         api_result = gb.lists.batch_subscribe(:id => list_id, :batch => [{:email => {:email => current_user ? current_user.email : params[:email]}}])
         if api_result["errors"].any?
-          {:error => true, :description => api_result["errors"].map{|error| error["error"]}.to_sentence}
           partial = current_user ? 'user_subscription_error' : 'subscription_error'
+          Rails.logger.debug('Newsletter Controller :13' + api_result["errors"].map{|error| error["error"]}.to_sentence})
+          {:error => true, :description => api_result["errors"].map{|error| error["error"]}.to_sentence}
         else
-          {:success => true, :description => "You've subscribed to our list. You'll soon receive an email to confirm your subscription."}
           partial = current_user ? 'user_subscription_success' : 'subscription_success'
+          {:success => true, :description => "You've subscribed to our list. You'll soon receive an email to confirm your subscription."}
         end
       else
-        {:error => true, :description => "You need to sign up"}
+        Rails.logger.debug('Newsletter Controller :19 sign up')
         partial = 'subscription_error'
+        {:error => true, :description => "You need to sign up"}
       end
       respond_to do |format|
         format.json { render :json => result }
