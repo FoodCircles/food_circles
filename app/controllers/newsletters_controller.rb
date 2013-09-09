76d5bef8035fd21ba3a7a6c676fcee5ed9a69020
Joe Woodward <woodward.jp@gmail.com>
2013-09-09 18:59:58 +0100

    tweaks to cc form

--- app/controllers/newsletters_controller.rb
+++ app/controllers/newsletters_controller.rb
@@ -9,14 +9,12 @@ class NewslettersController < ApplicationController
         api_result = gb.lists.subscribe({:id => list_id, :email => {:email => current_user ? current_user.email : params[:email]}, :merge_vars => {:FNAME => current_user ? current_user.name : params[:email], :LNAME => ''}, :double_optin => false})
         if api_result["errors"].present?
           partial = current_user ? 'user_subscription_error' : 'subscription_error'
-          Rails.logger.debug('Newsletter Controller :13' + api_result["errors"].map{|error| error["error"]}.to_sentence)
           {:error => true, :description => api_result["errors"].map{|error| error["error"]}.to_sentence}
         else
           partial = current_user ? 'user_subscription_success' : 'subscription_success'
           {:success => true, :description => "You've subscribed to our list. You'll soon receive an email to confirm your subscription."}
         end
       else
-        Rails.logger.debug('Newsletter Controller :19 sign up')
         partial = 'subscription_error'
         {:error => true, :description => "You need to sign up"}
       end
