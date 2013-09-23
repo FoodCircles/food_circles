b4d165f6d70683b4b02ea0e7e800eb4f6e862fb5
lacides <lcharris89@gmail.com>
2013-09-23 15:49:35 -0500

    organizers can submit events without a valid email address

--- app/controllers/organizers_controller.rb
+++ app/controllers/organizers_controller.rb
@@ -1,19 +1,9 @@
 class OrganizersController < ApplicationController
   def create
-  	if(params[:email])
-      if valid_email?(params[:email])
-        signup
-        render 'confirm'
-      else
-        flash[:error] = 'Invalid email address.'
-        render 'index'
-      end
-    end
-  end
-
-  def signup
     UserMailer.organizers_notify(params[:email], params[:location], params[:address], params[:date], params[:num_people], params[:occassion], params[:budget], params[:food_preferences], params[:donation], params[:feedback]).deliver
-    UserMailer.organizers_signup(params[:email]).deliver
+    if valid_email?(params[:email])
+      UserMailer.organizers_signup(params[:email]).deliver
+    end
 
     @n = Notification.create
     @n.content = "Email: #{params[:email]}, Location: #{params[:location]}, Address: #{params[:address]}, Date: #{params[:date]}, Number of People: #{params[:num_people]}, Occassion: #{params[:occassion]}, Budget: #{params[:budget]}, Food Preferences: #{params[:food_preferences]}, Donation: #{params[:donation]}, Feedback: #{params[:feedback]}"
@@ -21,6 +11,7 @@ class OrganizersController < ApplicationController
     @n.save
 
     @event = OpenStruct.new(params.except(:utf8, :authenticity_token))
+    render 'confirm'
   end
 
   def valid_email?(email)
