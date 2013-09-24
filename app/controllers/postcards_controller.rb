9c8ed5cc17be5e9021b382bd941d66a052303e3e
lacides <lcharris89@gmail.com>
2013-09-24 12:40:54 -0500

    enable timeline social share buttons

--- app/controllers/postcards_controller.rb
+++ app/controllers/postcards_controller.rb
@@ -1,34 +1,11 @@
 class PostcardsController < ApplicationController
   def create
     postcard = Postcard.new(params[:postcard])
-    facebook_sharing_uri = URI.parse "http://www.facebook.com/sharer/sharer.php"
-    twitter_sharing_uri = URI.parse "https://twitter.com/intent/tweet"
-
-    image_url =  URI.join "http://#{request.host}", "assets/", "logo_old.png"
-    
-    facebook_query = {
-      :s => 100,
-      :p => {
-        :url => restaurants_url,
-        :images => {0 => image_url.to_s},
-        :title => "I want #{postcard.restaurant_name} to be a Buy One, Feed One restaurant",
-        :summary => "Who's with me?"
-      }
-    }
-
-    twitter_query = {
-      :text => "I want #{postcard.restaurant_name} to be a Buy One, Feed One restaurant. #BuyOneFeedOne",
-      :url => restaurants_url
-    }
-
-    facebook_sharing_uri.query = facebook_query.to_query
-    twitter_sharing_uri.query = twitter_query.to_query
-
     render :json => {
       :success => true,
       :description => "We'll be sending a snail mail postcard to #{postcard.restaurant_name} shortly",
-      :facebook_sharing_uri => facebook_sharing_uri.to_s,
-      :twitter_sharing_uri => twitter_sharing_uri.to_s
+      :facebook_sharing_uri => view_context.facebook_share_url("I want #{postcard.restaurant_name} to be a Buy One, Feed One restaurant", "Who's with me?"),
+      :twitter_sharing_uri => view_context.twitter_share_url("I want #{postcard.restaurant_name} to be a Buy One, Feed One restaurant. #BuyOneFeedOne")
     }
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -54,5 +54,33 @@ module ApplicationHelper
       "Visa" => "visa"
     }[current_user_credit_card_data.type] || "generic"
   end
+
+  def facebook_share_url(title, summary, url = restaurants_url)
+    facebook_sharing_uri = URI.parse "http://www.facebook.com/sharer/sharer.php"
+    image_url =  URI.join "http://#{request.host}", "assets/", "logo_old.png"
+    facebook_query = {
+      :s => 100,
+      :p => {
+        :url => url,
+        :images => {0 => image_url.to_s},
+        :title => title,
+        :summary => summary
+      }
+    }
+    facebook_sharing_uri.query = facebook_query.to_query
+    facebook_sharing_uri.to_s
+  end
+
+  def twitter_share_url(text, url = restaurants_url)
+    twitter_sharing_uri = URI.parse "https://twitter.com/intent/tweet"
+
+    twitter_query = {
+      :text => text,
+      :url => url
+    }
+
+    twitter_sharing_uri.query = twitter_query.to_query
+    twitter_sharing_uri.to_s
+  end
 end
 
