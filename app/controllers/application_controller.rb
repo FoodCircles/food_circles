9580b43c3812b79cf3b1d9659e6a605aeb591b29
lacides <lcharris89@gmail.com>
2013-10-03 12:15:19 -0500

    after_sign_in_path fallback uses devise's stored_location_for method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -95,7 +95,7 @@ class ApplicationController < ActionController::Base
   end
 
   def after_sign_in_path_for(resource_or_scope)
-    request.env['omniauth.origin'] || request.referer || root_path 
+    request.env['omniauth.origin'] || stored_location_for(resource_or_scope) || root_path
   end
 
   def after_sign_up_path_for(resource)
