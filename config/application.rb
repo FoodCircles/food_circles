2455ecfb7d1af1fb58d5585d1161b6927ddf8be8
Mike King <vindictive27@gmail.com>
2013-11-04 23:42:41 -0500

    expire assets

--- config/application.rb
+++ config/application.rb
@@ -84,7 +84,7 @@ module Foodcircles
     config.assets.enabled = true
 
     # Version of your assets, change this if you want to expire all your assets
-    config.assets.version = '1.0'
+    config.assets.version = '1.1'
     config.middleware.use "PDFKit::Middleware", :print_media_type => true
 
     # config.paperclip_defaults = {
