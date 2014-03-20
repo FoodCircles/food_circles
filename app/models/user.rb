4dabc46561be4a469c1c0f865e7dd1fe4cc17921
Pablo Seibelt (Sicarul) <pabloseibelt@gmail.com>
2014-03-19 21:43:49 -0300

    Represent badges in rails admin

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,6 +17,7 @@ class User < ActiveRecord::Base
   has_many :reservations
   has_many :venues
   has_many :payments
+  has_many :user_badges
   has_many :notification_requests
   has_many :watched_venues, :through => :notification_requests, :source => :venue
   has_many :external_uids, :class_name => "ExternalUID", :dependent => :destroy
