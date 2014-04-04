FoodCircles/food_circles

commit 5a72bc4f86f79857e47568de1bf51ce16499c9c1

Deleting delays to avoid mail duplication

app/models/expiring_soon_checker.rb
@@ -16,7 +16,7 @@ class ExpiringSoonChecker
 
   def notify_users
     @payments.each do |payment|
-      UserMailer.delay.voucher_expiring_soon(payment.id)
+      UserMailer.voucher_expiring_soon(payment.id)
     end
   end
 

lib/tasks/daily_badges.rake
@@ -18,7 +18,7 @@ namespace :mailers do
     send_emails = UserBadge.find_all_by_sent_email(false)
 
     send_emails.each do |se|
-      UserMailer.delay.badge_email(se.user.email, badge.category, badge.title)
+      UserMailer.badge_email(se.user.email, badge.category, badge.title)
       se.sent_email = true
       se.save
     end

commit 9bbfeb9fdd691ba20b53d8e16d3365f9a1a5bd52

Commit 5a72bc4f86f79857e47568de1bf51ce16499c9c1 broke emails for expiring vouchers and badge emails, this fixes it. Sorry

app/models/expiring_soon_checker.rb
@@ -16,7 +16,7 @@ class ExpiringSoonChecker
 
   def notify_users
     @payments.each do |payment|
-      UserMailer.voucher_expiring_soon(payment.id)
+      UserMailer.voucher_expiring_soon(payment.id).deliver
     end
   end
 

lib/tasks/daily_badges.rake
@@ -18,7 +18,7 @@ namespace :mailers do
     send_emails = UserBadge.find_all_by_sent_email(false)
 
     send_emails.each do |se|
-      UserMailer.badge_email(se.user.email, badge.category, badge.title)
+      UserMailer.badge_email(se.user.email, badge.category, badge.title).deliver
       se.sent_email = true
       se.save
     end