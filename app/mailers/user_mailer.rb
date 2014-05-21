feb85419d39c1598698da3edb188e60d1e3e52e7
Jonathan Kumar <jtkumario@gmail.com>
2014-05-21 18:13:45 -0400

    testing monthly report send and fixing voucher bugs

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -36,8 +36,8 @@
     @payment = r
     mail(
       :to => @user.email,
-      :subject => "Got your Voucher for #{r.offer.venue.name}",
-      :reply_to => 'used@inbound.foodcircles.net'
+      :subject => "Your Voucher for #{r.offer.venue.name}",
+      :reply_to => 'hey@joinfoodcircles.org'
     )
   end
 
@@ -88,7 +88,7 @@
   end
 
   def organizers_notify(email, location, address, date, num_diners, occassion, budget, food_preferences, donation, feedback)
-    mail(:to => 'bshell@joinfoodcircles.org', cc: ADMIN_EMAIL, :subject => "New Organizers Request", :body => "An organizer would like to join FoodCircles. They will be dining #{location} on #{date} with #{num_diners} guests. The occasion is #{occassion} with a budget of #{budget}. Their food preferences include #{food_preferences}. They would like to donate #{donation}. They provided the following feedback: #{feedback}. Please contact them at #{email}")
+    mail(:to => 'riley@joinfoodcircles.org', cc: ADMIN_EMAIL, :subject => "New Organizers Request", :body => "An organizer would like to join FoodCircles. They will be dining #{location} on #{date} with #{num_diners} guests. The occasion is #{occassion} with a budget of #{budget}. Their food preferences include #{food_preferences}. They would like to donate #{donation}. They provided the following feedback: #{feedback}. Please contact them at #{email}")
   end
 
   def notification_about_available_vouchers(user, venue)
@@ -100,7 +100,7 @@
 
   def monthly_invoice(venue, months_before = 1)
     @calculations = Calculations::Monthly.new(venue, months_before)
-    mail(:to => venue.email, cc: ADMIN_EMAIL, :subject => "Our Report for #{@calculations.month}")
+    mail(:to => ADMIN_EMAIL, :subject => "Our Report for #{@calculations.month}")
   end
 
   def postcard_notification(postcard)
