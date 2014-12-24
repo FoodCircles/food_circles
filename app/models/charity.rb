e54977d692500882903d0310eac32c9e2ebbd746
Pablo Seibelt (Sicarul) <pabloseibelt@gmail.com>
2014-12-24 00:56:23 -0300

    Added operator with multiplier

--- app/models/charity.rb
+++ app/models/charity.rb
@@ -51,6 +51,9 @@ class Charity < ActiveRecord::Base
     uf = self.use_funds.dup
     uf['%amt%'] = amt.to_s if uf.include? '%amt%'
 
+    amt_mult = uf.match('\%amt:([0-9\.]+)%')
+    uf[amt_mult[0]] = (amt.to_f*amt_mult[1].to_f).to_s unless amt_mult.nil?
+
     if uf.include? '%s%'
       if amt > 1
         uf['%s%'] = 's'
