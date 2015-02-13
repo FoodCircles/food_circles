06063edea1a97051e9559adb1a0d4bf272b3bfcf
Pablo Seibelt <pabloseibelt@gmail.com>
2015-02-13 16:31:41 -0300

    Added weekly goal stats

--- app/controllers/api/venues_controller.rb
+++ app/controllers/api/venues_controller.rb
@@ -1,5 +1,8 @@
 class Api::VenuesController < ApplicationController
   def show
-    render :json => {:error => false, :content => Venue.all.as_json(lat: params[:lat], lon: params[:lon], all: true) }
+    render :json => {:error => false,
+      :content => Venue.all.as_json(lat: params[:lat], lon: params[:lon], all: true),
+      :people_aided => weekly_progress[:current_progress],
+      :weekly_goal => weekly_progress[:adjusted_total]}
   end
 end
