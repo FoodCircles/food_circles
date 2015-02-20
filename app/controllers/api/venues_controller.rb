3e0aa9ab812cecf4dd17250a2d4b05c351f16f9e
Pablo Seibelt <pabloseibelt@gmail.com>
2015-02-20 11:31:02 -0300

    Added total people aidd

--- app/controllers/api/venues_controller.rb
+++ app/controllers/api/venues_controller.rb
@@ -2,6 +2,7 @@ class Api::VenuesController < ApplicationController
   def show
     render :json => {:error => false,
       :content => Venue.all.as_json(lat: params[:lat], lon: params[:lon], all: true),
+      :total_people_aided => total_meals,
       :people_aided => weekly_progress[:current_progress],
       :weekly_goal => weekly_progress[:adjusted_total]}
   end
