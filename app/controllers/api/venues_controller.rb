3fa7ee7e1807a736222cc2aa3c3306ff5f520478
Qays H. Poonawala <qayshp@gmail.com>
2015-06-28 11:46:36 -0700

    Pass venue as object to Calculations, JSON-ify for output.

--- app/controllers/api/venues_controller.rb
+++ app/controllers/api/venues_controller.rb
@@ -8,12 +8,12 @@ class Api::VenuesController < ApplicationController
   end
 
   def homeless
-    venue = Venue.where(device_id: params[:device_id]).first.as_json
+    venue = Venue.where(device_id: params[:device_id]).first
     calculations = Calculations::All.new(venue)
     balance = calculations.total_purchases_by_charities[1].round
     render :json => {
       :error => false,
-      :content =>  venue,
+      :content =>  venue.as_json,
       :total_people_aided => total_meals,
       :people_aided => weekly_progress[:current_progress],
       :weekly_goal => weekly_progress[:adjusted_total],
